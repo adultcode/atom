ffd042e2ab9a110b8e360dabca66d5d68df10eb8
hharddy <hharddy@yahoo.com>
2025-05-09 20:07:18 +0330

    final result

--- lib/widgets/result_page.dart
+++ lib/widgets/result_page.dart
@@ -10,20 +10,32 @@ class ResultsScreen extends StatelessWidget {
 
   int get totalScore => answers.fold(0, (sum, answer) => sum + (answer ?? 0));
 
+  String _finalResult(int point){
+    if(point<90) return "شما از اصحاب شمال هستید";
+    else if(point>=90 && point< 110) return "شما اهل یمین هستید و از مومنان صالح بهشتی";
+    else  return "شما از سابقون السابقون هستید و نزدیکترین جایگاه را به خداوند در بهشت دارید";
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('نتایج')),
+      appBar: AppBar(title: Text('نتایج'),centerTitle: true,),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 20,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
+
             Text(
-              'نتایج شما:',
+              'نتیجه: ${_finalResult(totalScore)}',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                textDirection: TextDirection.rtl
             ),
-            SizedBox(height: 20),
+            Text(
+              'پاسخ های شما:',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                textDirection: TextDirection.rtl
+            ),
             ListView.builder(
               shrinkWrap: true,
               physics: NeverScrollableScrollPhysics(),
@@ -32,10 +44,12 @@ class ResultsScreen extends StatelessWidget {
                 return Padding(
                   padding: const EdgeInsets.symmetric(vertical: 8.0),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+
                     children: <Widget>[
                       Text(
                         '${index + 1}. ${questionsWithAnswers[index].question}',
+                        textDirection: TextDirection.rtl,
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                       SizedBox(height: 5),
@@ -45,11 +59,7 @@ class ResultsScreen extends StatelessWidget {
                 );
               },
             ),
-            SizedBox(height: 20),
-            Text(
-              'امتیاز کل: $totalScore',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
+
           ],
         ),
       ),
